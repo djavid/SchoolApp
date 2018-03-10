c89fbb5657e2b44086e40ed1aadddfae6f25521d
futures-dev <futures_dev@outlook.com>
2018-03-10 19:46:53 +0300

    Fix Issue #13

--- app/src/main/java/com/djavid/schoolapp/view/activity/EnterCodeActivity.java
+++ app/src/main/java/com/djavid/schoolapp/view/activity/EnterCodeActivity.java
@@ -58,9 +58,6 @@ public class EnterCodeActivity extends AppCompatActivity {
         mEnterCodeButton.setOnClickListener(view -> attemptLogin());
 
         mProgressView = findViewById(R.id.login_progress);
-
-        //todo
-        showDashboard();
     }
 
     @Override
